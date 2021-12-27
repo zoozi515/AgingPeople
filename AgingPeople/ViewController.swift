2c5fa120819e5337ada65c796a20a545c18dd2b6
Zahra Al-Jafar <memo.jks357@hotmail.com>
2021-12-27 22:48:41 +0000

    Ageing People Version #1 (using title subtitle)

--- AgingPeople/ViewController.swift
+++ AgingPeople/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let listOfNames = ["George","Fran", "Joe", "Helda", "Winifred", "Zed", "Sara", "Jeffy", "Abraham", "Anna", "Melinda"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonDetCell", for: indexPath)
+        
+        //add text to the cell using custom design
+        cell.textLabel?.text = self.listOfNames[indexPath.row]
+        cell.detailTextLabel?.text = "\(arc4random_uniform(90)) years old"
 
+        return cell
+    }
 }
 
